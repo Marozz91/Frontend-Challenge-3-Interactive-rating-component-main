f108d57c1f674c862c63e35eb14891a2ee242896
Marwa <marwa.nazy@hotmail.com>
2023-07-30 02:29:56 +0300

    Modified

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 
 (() => {
 
-    const btn = document.getElementById("btn");
-    const rates = document.querySelectorAll(".number");
-    const para = document.querySelector(".rating");
+    const btnSubmit = document.getElementById("btnSubmit");
+    const buttons = document.querySelectorAll(".btn");
+    const para = document.getElementById("rating");
 
-
-
-    btn.addEventListener("click", function () {
+    btnSubmit.addEventListener("click", function () {
 
         document.querySelector(".thank-you-state").classList.remove("hidden");
         document.querySelector(".section-content").style.display = "none";
@@ -15,11 +13,11 @@
     });
 
 
-    rates.forEach(rate => {
+    buttons.forEach(btn => {
 
-        rate.addEventListener("click", function () {
+        btn.addEventListener("click", function () {
 
-            const text = rate.innerText;
+            const text = btn.innerText;
             para.innerHTML = `You selected ${text} out of 5`
 
         });
