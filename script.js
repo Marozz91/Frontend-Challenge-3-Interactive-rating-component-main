f3a39546c6a739d72356edc6c318e34c60772966
Marwa <marwa.nazy@hotmail.com>
2023-07-30 01:42:58 +0300

    Modified

--- script.js
+++ script.js
@@ -2,66 +2,28 @@
 (() => {
 
     const btn = document.getElementById("btn");
+    const rates = document.querySelectorAll(".number");
+    const para = document.querySelector(".rating");
 
 
-    btn.addEventListener("click", () => {
 
-        document.querySelector(".thank-you-state").style.visibility = "visible";
-        document.querySelector(".section-content").style.visibility = "hidden";
+    btn.addEventListener("click", function () {
 
-    });
-
-
-    const submitBtn1 = document.querySelector(".number1");
-    const submitBtn2 = document.querySelector(".number2");
-    const submitBtn3 = document.querySelector(".number3");
-    const submitBtn4 = document.querySelector(".number4");
-    const submitBtn5 = document.querySelector(".number5");
-
-    submitBtn1.addEventListener("click", function () {
-
-        const text = submitBtn1.innerText;
-        const para = document.querySelector(".rating");
-        para.innerHTML = `You selected ${text} out of 5`
+        document.querySelector(".thank-you-state").classList.remove("hidden");
+        document.querySelector(".section-content").style.display = "none";
 
     });
 
 
-    submitBtn2.addEventListener("click", function () {
+    rates.forEach(rate => {
 
-        const text = submitBtn2.innerText;
-        const para = document.querySelector(".rating");
-        para.innerHTML = `You selected ${text} out of 5`
+        rate.addEventListener("click", function () {
 
+            const text = rate.innerText;
+            para.innerHTML = `You selected ${text} out of 5`
+
+        });
     });
-
-
-    submitBtn3.addEventListener("click", function () {
-
-        const text = submitBtn3.innerText;
-        const para = document.querySelector(".rating");
-        para.innerHTML = `You selected ${text} out of 5`
-
-    });
-
-    submitBtn4.addEventListener("click", function () {
-
-        const text = submitBtn4.innerText;
-        const para = document.querySelector(".rating");
-        para.innerHTML = `You selected ${text} out of 5`
-
-    });
-
-
-    submitBtn5.addEventListener("click", function () {
-
-        const text = submitBtn5.innerText;
-        const para = document.querySelector(".rating");
-        para.innerHTML = `You selected ${text} out of 5`
-
-    });
-
-
 
 
 })();
